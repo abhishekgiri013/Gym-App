237f28ed3b1b6234eb219c2aa2bfc1bc86b03813
ABHISHEK GIRI <98091185+abhishekgiri013@users.noreply.github.com>
2025-02-16 01:43:54 +0530

    first commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      "© 2025 Abhishek Giri – Powered by caffeine, late-night coding, and questionable life choices. 🚀"
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Frontend/src/components/WorkoutSession.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const WorkoutSession = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WOKROUT SESSION</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Esse illum
+          expedita enim inventore repellat excepturi assumenda debitis
+          exercitationem.
+        </p>
+        <img src="/pic1.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMP</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et fuga eos
+          aspernatur dolorem expedita numquam. Ut, odit.
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+              fugiat ab sed quod praesentium temporibus?
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+              fugiat ab sed quod praesentium temporibus?
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+              fugiat ab sed quod praesentium temporibus?
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+              fugiat ab sed quod praesentium temporibus?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSession;
